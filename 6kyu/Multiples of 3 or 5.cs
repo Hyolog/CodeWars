054d93652fbbdc583f61f18fcdd135529cbc92ca
Hyolo <soejdtodxn@gmail.com>
2020-04-02 17:29:13 +0900

    [6kyu] Multiples of 3 or 5

---
+++ 6kyu/Multiples of 3 or 5.cs
@@ -0,0 +1,23 @@
+//https://www.codewars.com/kata/514b92a657cdc65150000006/csharp
+
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        return GetSumOfMultiples(3, value) + GetSumOfMultiples(5, value) - GetSumOfMultiples(15, value);
+    }
+
+    public static int GetSumOfMultiples(int value, int limit)
+    {
+        int tempNum = value;
+        int resultNum = 0;
+
+        while (tempNum < limit)
+        {
+            resultNum += tempNum;
+            tempNum += value;
+        }
+
+        return resultNum;
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## 6 kyu
 * [Find the odd int](6kyu/Find%20the%20odd%20int.cs)
-* [Replace With Alphabet Position] (6kyu/Replace%20With%20Alphabet%20Position.cs)
+* [Replace With Alphabet Position](6kyu/Replace%20With%20Alphabet%20Position.cs)
+* [Multiples of 3 or 5](6kyu/Multiples%20of%203%20or%205.cs)
