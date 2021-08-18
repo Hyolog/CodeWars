a0a1370231fe54283cd948a673baf1f3d4075390
dhkang <soejdtodxn@naver.com>
2021-08-19 00:03:29 +0900

    MSTest project 추가

---
+++ CodeWars/CodeWars/DefaultTemplate.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars
+{
+    /// <see cref=""/>
+    [TestClass]
+    public class DefaultTemplate
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
